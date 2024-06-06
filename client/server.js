7f5afb10482c70e0bc4a813b2d9cc257dcc4ccf9
KasmoProject <radhikarnair2024@yahoo.com>
2024-06-06 13:28:11 -0400

    Update server.js

--- client/server.js
+++ client/server.js
@@ -4,14 +4,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 
-// server.js or index.js
-console.log('Environment Variables:');
-console.log(`REACT_APP_API_BASE_URL: ${process.env.REACT_APP_API_BASE_URL}`);
-console.log(`REACT_APP_AUTHENTICATION_SERVICE_URL: ${process.env.REACT_APP_AUTHENTICATION_SERVICE_URL}`);
-console.log(`REACT_APP_COMMON_DATA_SERVICE_URL: ${process.env.REACT_APP_COMMON_DATA_SERVICE_URL}`);
-console.log(`REACT_APP_PAYMENT_SERVICE_URL: ${process.env.REACT_APP_PAYMENT_SERVICE_URL}`);
-console.log(`REACT_APP_SEARCH_SUGGESTION_SERVICE_URL: ${process.env.REACT_APP_SEARCH_SUGGESTION_SERVICE_URL}`);
-console.log(`REACT_APP_ENVIRONMENT: ${process.env.REACT_APP_ENVIRONMENT}`);
+
 
 
 
@@ -23,6 +16,14 @@ app.get('/ping', function (req, res) {
     return res.send('pong');
 });
 app.get('/*', function (req, res) {
+    // server.js or index.js
+console.log('Environment Variables:');
+console.log(`REACT_APP_API_BASE_URL: ${process.env.REACT_APP_API_BASE_URL}`);
+console.log(`REACT_APP_AUTHENTICATION_SERVICE_URL: ${process.env.REACT_APP_AUTHENTICATION_SERVICE_URL}`);
+console.log(`REACT_APP_COMMON_DATA_SERVICE_URL: ${process.env.REACT_APP_COMMON_DATA_SERVICE_URL}`);
+console.log(`REACT_APP_PAYMENT_SERVICE_URL: ${process.env.REACT_APP_PAYMENT_SERVICE_URL}`);
+console.log(`REACT_APP_SEARCH_SUGGESTION_SERVICE_URL: ${process.env.REACT_APP_SEARCH_SUGGESTION_SERVICE_URL}`);
+console.log(`REACT_APP_ENVIRONMENT: ${process.env.REACT_APP_ENVIRONMENT}`);
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
