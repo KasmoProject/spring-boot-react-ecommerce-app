1a749afd73917cf4eb794f9f58818dc800f2f2bb
KasmoProject <radhikarnair2024@yahoo.com>
2024-06-06 10:24:47 -0500

    Update start-all.sh

--- start-all.sh
+++ start-all.sh
@@ -3,7 +3,9 @@
 #script to update the ec2 instance public ip dynamically.
 PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 export PUBLIC_IP
-echo(PUBLIC_IP)
+echo 'PUBLIC_IP'
+echo $PUBLIC_IP
+
 
 # Stop and delete the containers
 #docker-compose down
