c9012f20faef8f307d35953c8d72a8de8e9fdf33
KasmoProject <radhikarnair2024@yahoo.com>
2024-06-06 14:04:13 -0400

    Update set-env.sh
    echo added

--- client/set-env.sh
+++ client/set-env.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Starting set-env.sh script..."
+
 # Fetch the public IP address from EC2 metadata
 PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+
+echo "Public IP: $PUBLIC_IP"
 
 # Export the public IP as environment variables
 export REACT_APP_API_BASE_URL="http://$PUBLIC_IP:port"
@@ -10,11 +14,10 @@ export REACT_APP_COMMON_DATA_SERVICE_URL="http://$PUBLIC_IP:9000"
 export REACT_APP_PAYMENT_SERVICE_URL="http://$PUBLIC_IP:9050"
 export REACT_APP_SEARCH_SUGGESTION_SERVICE_URL="http://$PUBLIC_IP:10000"
 
-
-# Ensure the target directory exists- sumana - changes
+# Ensure the target directory exists
 mkdir -p /usr/src/app/client
 
-# Write the environment variables to a .env file (optional, if needed by the application)
+# Write the environment variables to a .env file
 cat <<EOF > /usr/src/app/client/.env
 REACT_APP_STRIPE_PUBLISH_KEY=pk_test_5dsf4534jkmn4nm345QSQK0WjEJKx1PNH3mJxeUkA45345345Vcxyh5bDuulBildNrp3MWn005xEkAdJ4
 REACT_APP_GOOGLE_AUTH_CLIENT_ID=357808142500-sdfsndln4oh5345435.apps.googleusercontent.com
@@ -37,6 +40,8 @@ REACT_APP_SEARCH_SUGGESTION_SERVICE_URL=http://$PUBLIC_IP:10000
 
 REACT_APP_ENVIRONMENT=<prod/dev>
 EOF
+
+echo "Environment variables written to /usr/src/app/client/.env"
 
 # Execute the original command
 exec "$@"
